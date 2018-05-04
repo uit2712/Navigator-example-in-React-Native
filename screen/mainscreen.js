b202c4e822aba744975cdfb4440acc04a467cb2e
Quang Vĩ Saitama <phamquangvi456@gmail.com>
2018-05-04 19:13:17 +0700

    Updated on 04-05-2018
    Updating source code

---
+++ screen/mainscreen.js
@@ -0,0 +1,47 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+} from 'react-native';
+
+export default class MainScreen extends Component<{}> {
+  static navigationOptions = {
+    title: 'Main Screen',
+  };
+
+  render() {
+    const { navigate } = this.props.navigation
+
+    return (
+      <View style={styles.container}>
+        <Text>This is main screen :D :D</Text>
+
+        <TouchableOpacity onPress={() => navigate('Screen1')}>
+          <Text>Go to first screen!!</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity onPress={() => navigate('Screen2')}>
+          <Text>Go to second screen!!</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+});
---
+++ screen/screen1.js
@@ -0,0 +1,36 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+export default class Screen1 extends Component<{}> {
+  static navigationOptions = {
+    title: 'First Screen',
+  };
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>This is first screen :D :D</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+});
